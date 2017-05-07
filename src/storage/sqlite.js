e6e1969ca6b71c2867e950178a5a33734aeaee08
Stephen Abrahamsen <stephen@dispatch.me>
2017-05-07 04:57:44 -0400

    Added database initialize functions

--- src/storage/sqlite.js
+++ src/storage/sqlite.js
@@ -1,11 +1,11 @@
 import sqlite3 from 'sqlite3';
 
-var db = new sqlite3.Database('chinese.db');
+var db = new sqlite3.Database('./chinese.db');
 
 export function execute(statement) {
   return new Promise((resolve) => {
     return db.all(statement, (err, results) => {
-      resolve(results);
+      resolve(results, err);
     });
   });
 }
